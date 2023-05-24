00b5a5507f089c6e9cec30f7c4ee04e7a8f6c895
Natã Girotto <natagirotto@hens.com.br>
2023-05-24 17:44:12 -0300

    utilizando params

---
+++ 6_EXPRESS/6_param_url/index.js
@@ -0,0 +1,41 @@
+import Express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+const app = Express();
+const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+const basePath = path.join(__dirname, "templates");
+const checkAuth = (req, res, next) => {
+    req.authStatus = true;
+    if (req.authStatus) {
+        console.log("User is authenticated");
+        next();
+    } else {
+        console.log("User is not authenticated");
+        next()
+    }
+}
+
+app.use(checkAuth);
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+
+    //leitura da tabela users, resgate do usuário com o id
+    console.log(`User id: ${id}`);
+
+    res.sendFile(`${basePath}/users.html`);
+
+}
+);
+app.get("/", (req, res) => {
+
+    res.sendFile(`${basePath}/index.html`);
+
+}
+);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+}
+);
