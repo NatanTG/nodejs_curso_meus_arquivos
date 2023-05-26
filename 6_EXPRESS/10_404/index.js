1e5c72b4a66f8bb6a72c1a57546404e58ffc482b
Natã Girotto <natagirotto@hens.com.br>
2023-05-25 21:00:07 -0300

    adicionado tarefa03

---
+++ 6_EXPRESS/10_404/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const { router } = require("./users/index.js");
+
+const app = express();
+const port = 3000;
+const basePath = path.join(__dirname, "templates");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join("public")));
+app.use("/users", router);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(basePath, "index.html"));
+});
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(basePath, "404.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
