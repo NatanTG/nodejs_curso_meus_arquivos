77c5a7fc144f1455bd9309684a88bd4c06c46f10
Natã Girotto <natagirotto@hens.com.br>
2023-06-12 18:15:57 -0300

    atv: 08 deleteando dados

---
+++ 10_MVC/8_deletando_dados/controllers/TaskController.js
@@ -0,0 +1,42 @@
+import Task from '../models/Task.js';
+
+export default class TaskController {
+
+    static createTask(req, res) {
+        res.render('tasks/create');
+    }
+
+    static async createTaskSave(req, res) {
+        const task = { title: req.body.title, description: req.body.description, done: false }
+
+        //validações
+        //processar dados
+
+        await Task.create(task);
+        res.redirect('/tasks');
+    }
+
+
+
+    static async showTask(req, res) {
+
+        const tasks = await Task.findAll({ raw: true })
+
+        res.render('tasks/all', { tasks });
+
+
+    }
+
+    static async removeTask(req, res) {
+        const id = req.body.id;
+
+        await Task.destroy({
+            where: {
+                id: id
+            }
+        });
+
+        res.redirect('/tasks');
+    }
+
+}
