05cdac2632c0acb93d0cf72e874944ebcb749ef0
Natã Girotto <natagirotto@hens.com.br>
2023-06-30 15:44:29 -0300

    adicionado: instalando mongoose

---
+++ 13_MONGOOSE/1_instalacao/routes/productRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import ProductControllers from '../controllers/ProductControllers.js';
+
+const router = express.Router();
+
+router.get("/create", ProductControllers.createProduct);
+router.post("/create", ProductControllers.createProductPost);
+router.get('/edit/:id', ProductControllers.editProduct);
+router.post('/products/edit', ProductControllers.editProductPost);
+router.post('/remove/:id', ProductControllers.removeProduct)
+router.get('/products/:id', ProductControllers.getProduct);
+router.get("/", ProductControllers.showProducts);
+
+export default router;
