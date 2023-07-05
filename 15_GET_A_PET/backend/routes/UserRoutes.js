4ae0a978ba9e67c7b77615d69206af53bf9f72ff
Natã Girotto <natagirotto@hens.com.br>
2023-07-05 17:08:49 -0300

    add: rota login; rota checkuser

--- 15_GET_A_PET/backend/routes/UserRoutes.js
+++ 15_GET_A_PET/backend/routes/UserRoutes.js
@@ -3,5 +3,7 @@ import UserController from '../controllers/UserController.js';
 
 const router = Router();
 router.post('/register', UserController.register);
+router.post("/login", UserController.login);
+router.get("/checkuser", UserController.checkUser);
 
 export default router;
