d201ead5dcff507631d426456dd2b885442f1d0b
Natã Girotto <natagirotto@hens.com.br>
2023-07-07 14:50:24 -0300

    bug: fixed bug import get-token in verify-token

--- 15_GET_A_PET/backend/helpers/verify-token.js
+++ 15_GET_A_PET/backend/helpers/verify-token.js
@@ -1,14 +1,23 @@
 import jwt from 'jsonwebtoken';
-import getToken from './get-token';
+import getToken from './get-token.js';
 
 //middleware to check if token is valid
 const checkToken = async (req, res, next) => {
-    const token = getToken(req)
 
     if (!req.headers.authorization) { return res.status(401).json({ error: "Acesso negado!" }) };
 
+    const token = getToken(req)
 
     if (!token) { return res.status(401).json({ error: "Acesso negado!" }) };
+
+    try {
+        const verified = jwt.verify(token, 'nossosecret');
+        req.user = verified;
+        next();
+    }
+    catch (err) {
+        res.status(400).json({ error: "Token inválido!" })
+    }
 
 }
 
