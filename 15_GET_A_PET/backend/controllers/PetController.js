71b6a5fbc85883473e1c699247811c31eb0055de
Natã Girotto <natagirotto@hens.com.br>
2023-07-13 14:18:03 -0300

    add: função removePetById; rota removePetById;
    coment:
    foi utilizado Pet.deleteOne({ _id: id }) invés de pet.findByIdAndDelete(id)

--- 15_GET_A_PET/backend/controllers/PetController.js
+++ 15_GET_A_PET/backend/controllers/PetController.js
@@ -123,5 +123,40 @@ export default class PetController {
             pet: pet,
         })
     }
+
+    static async removePetById(req, res) {
+        const id = req.params.id
+
+        // check if id is valid
+        if (!isValidObjectId(id)) {
+            res.status(422).json({ message: 'ID inválido!' })
+            return
+        }
+
+        // check if pet exists
+        const pet = await Pet.findOne({ _id: id })
+
+        if (!pet) {
+            res.status(404).json({ message: 'Pet não encontrado!' })
+            return
+        }
+
+        // check if user is the owner of the pet
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        if (pet.user._id.toString() !== user._id.toString()) {
+            res.status(401).json({ message: 'Você não tem permissão para remover este pet!' })
+            return
+        }
+
+        try {
+            await Pet.deleteOne({ _id: id })
+
+            res.status(200).json({ message: 'Pet removido com sucesso!' })
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
+    }
 }
 
--- 15_GET_A_PET/backend/routes/PetRoutes.js
+++ 15_GET_A_PET/backend/routes/PetRoutes.js
@@ -12,6 +12,6 @@ router.get('/', PetController.getAll);
 router.get('/mypets', PetController.getAllUserPets);
 router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions);
 router.get('/:id', PetController.getPetById)
-
+router.delete('/:id', verifyToken, PetController.removePetById)
 
 export default router;
