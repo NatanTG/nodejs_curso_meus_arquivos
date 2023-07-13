a31812b0e972e412abd381dca73bc1e6ff608f1f
Natã Girotto <natagirotto@hens.com.br>
2023-07-13 15:04:58 -0300

    add: função updatePet; rota updatePet;
    mod:
    pet.images.push(image.filename) do create do PetController.js

--- 15_GET_A_PET/backend/routes/PetRoutes.js
+++ 15_GET_A_PET/backend/routes/PetRoutes.js
@@ -13,5 +13,6 @@ router.get('/mypets', PetController.getAllUserPets);
 router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions);
 router.get('/:id', PetController.getPetById)
 router.delete('/:id', verifyToken, PetController.removePetById)
+router.patch('/:id', verifyToken, imageUpload.array('images'), PetController.updatePet)
 
 export default router;
