ca7ed1e9a396bbbcb75cb25b8526895b659a5e86
Natã Girotto <natagirotto@hens.com.br>
2023-07-14 12:48:16 -0300

    add: rota schedule; rota concludeAdoption;

--- 15_GET_A_PET/backend/routes/PetRoutes.js
+++ 15_GET_A_PET/backend/routes/PetRoutes.js
@@ -14,5 +14,6 @@ router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions);
 router.get('/:id', PetController.getPetById)
 router.delete('/:id', verifyToken, PetController.removePetById)
 router.patch('/:id', verifyToken, imageUpload.array('images'), PetController.updatePet)
-
+router.patch('/schedule/:id', verifyToken, PetController.schedule);
+router.patch('/conclude/:id', verifyToken, PetController.concludeAdoption);
 export default router;
