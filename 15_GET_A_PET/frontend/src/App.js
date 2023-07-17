cddbddfe9aa1de39854735634feae7e79b0a2abd
Natã Girotto <natagirotto@hens.com.br>
2023-07-17 15:08:31 -0300

    add: footer; navbar; import navbar, footer in app.js;

--- 15_GET_A_PET/frontend/src/App.js
+++ 15_GET_A_PET/frontend/src/App.js
@@ -1,15 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+/* Components */
+import Navbar from '../src/components/layout/Navbar.js'
+import Footer from '../src/components/layout/Footer.js'
+/* Pages */
 import Login from './components/pages/Auth/Login'
 import Register from './components/pages/Auth/Register'
 import Home from './components/pages/Home'
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" exact element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
